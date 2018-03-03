ea6876a19f266ece608071ba78d37642a6baa733
Franz Justin Buenaventura <franz_buenaventura@hotmail.com>
2018-03-03 12:51:22 -0800

    update

--- app/plain.py
+++ app/plain.py
@@ -215,4 +215,4 @@ def move():
 # Expose WSGI app (so gunicorn can find it)
 application = bottle.default_app()
 if __name__ == '__main__':
-    bottle.run(application, host=os.getenv('IP', '0.0.0.0'), port=os.getenv('PORT', '8080'))
+    bottle.run(application, host=os.getenv('IP', '192.168.97.179'), port=os.getenv('PORT', '8080'))
